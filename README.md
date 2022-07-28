d9a6b8d0d8d64197deefdbcf3534c01a3099631b
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2022-07-28 09:45:10 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # ap-gridworld-3d-engine
-Solution to APCS 3d Gridworld engine lab as described [here](https://docs.google.com/document/d/1MLoiBiA2aGNJt47wUIgn6nQSzdvB5Oo5CMJqowSeZwI/edit?usp=sharing).
+A fully functioning 3d engine using the APCS Gridworld interface as a display.
+Solution to APCS Gridworld assignment as described [here](https://docs.google.com/document/d/1MLoiBiA2aGNJt47wUIgn6nQSzdvB5Oo5CMJqowSeZwI/edit?usp=sharing).
 
 ![Sample execution](resources/sample.png)
+
+## Features
+- Basic shading with customizable lights and meshes
+- .OBJ importer to preview any models
+- Proper depth buffering
+- UV mapping and texturing
+
+## How to use
+Scenes can be configured in the `MainRunner.java` class. A complete demonstration of importing and
+initializing models can be found in the `main` function.
+
+Place any textures and .OBJ files inside the `resources` folder.
+
+.OBJ files can easily be exported from any 3d editing program and imported to the engine.
+If using Blender, ensure that the export includes UV data and "Keep Vertex Order" is enabled.
+Textures must be PNG-32.
--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,31 +1,41 @@
 import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class MainRunner {
 
 	public static void main(String[] args) throws IOException {
+		// Create the screen
+		ScreenWorld screen = new ScreenWorld(100, 100);
+
+		// Initialize meshes and add them to the scene
 		Mesh cube = ObjImporter.importObj(new Scanner(new File("resources/shapes/cube.obj")));
-		BufferedImage b =ImageIO.read(Objects.requireNonNull(MainRunner.class.getResource("/textures/cube.png")));
+		cube.setTranslation(new Vector(-.6,.2,-1.7));
+		BufferedImage b = ImageIO.read(Objects.requireNonNull(MainRunner.class.getResource("/textures/cube.png")));
 		cube.setTexture(b);
 
 		Mesh pot = ObjImporter.importObj(new Scanner(new File("resources/shapes/teapot.obj")));
+		pot.setTranslation(new Vector(1.2,-.5,-3.6));
 
-		cube.setTranslation(new Vector(-.1,.2,-1.5));
-		pot.setTranslation(new Vector(.5,-.5,-3.8));
+		Mesh monkey = ObjImporter.importObj(new Scanner(new File("resources/shapes/monkey.obj")));
+		monkey.setTranslation(new Vector(-.2,-.4,-1.55));
 
-		ScreenWorld screen = new ScreenWorld(100, 100);
-		screen.getScene().getMeshes().add(pot);
-		screen.getScene().getMeshes().add(cube);
+		List<Mesh> meshes = screen.getScene().getMeshes();
+		meshes.add(pot);
+		meshes.add(cube);
+		meshes.add(monkey);
 
-		// screen.getScene().getLights().add(new Light(new Vector(-1, -.5, .5), new Color(0, 120, 255), 0.35));
-		screen.getScene().getLights().add(new Light(new Vector(1, -.5, .5), new Color(255, 120, 0), 0.35));
-		screen.getScene().getLights().add(new Light(new Vector(0, .2, 1), Color.WHITE, 0.99));
+		// Create lights and add them to the scene
+		List<Light> lights = screen.getScene().getLights();
+		lights.add(new Light(new Vector(-1, -.5, .5), new Color(0, 120, 255), 0.35));
+		lights.add(new Light(new Vector(1, -.5, .5), new Color(255, 120, 0), 0.35));
+		lights.add(new Light(new Vector(0, .2, 1), Color.WHITE, 0.99));
 
 		screen.show();
 	}
